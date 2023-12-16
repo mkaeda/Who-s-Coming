22a2aea5c81d513c8f5e6662d7a9e312bf096b62
Jason Osmond <jasonosmond2001@gmail.com>
2023-12-16 02:45:12 -0700

    Added EventDetails Page
    Color is weird on RSVP, I cant find out why

--- whos-coming/src/App.js
+++ whos-coming/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Form from './components/EventForm.js';
+import EventForm from './components/EventForm.js';
+import EventDetails from './components/EventDetails.js';
 import './App.css'; // Assuming you have an App.css for your styles
 
 function App() {
     return (
         <div className="App">
-            <Form />
+            {/* <EventForm/> */}
+            <EventDetails/>
         </div>
     );
 }
